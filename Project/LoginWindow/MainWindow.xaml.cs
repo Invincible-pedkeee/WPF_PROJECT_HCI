26d2acdbee5c7e31a33dd611656fbf114959a916
Predrag Radanovic <154592653+Invincible-pedkeee@users.noreply.github.com>
2026-04-07 01:44:08 +0200

    init cpmmit

---
+++ Project/LoginWindow/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BasketballBallBrandsCMS.Helpers;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LoginWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppPaths.EnsureDirectoriesExist();
+           // MessageBox.Show(AppPaths.ResourcesFolder);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
