0faf402256a24f4b27fbe451ffce6b5410ef053c
Predrag Radanovic <154592653+Invincible-pedkeee@users.noreply.github.com>
2026-04-21 14:49:46 +0200

    recovery

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace BasketballBallBrandsCMS
+{
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var loginWindow = new Views.LoginWindow();
+            loginWindow.Show();
+        }
+    }
+}
